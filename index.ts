5e92df39d49906427cab8d6d2b1981eba1895866
coreyalix <coreyalix@hotmail.com>
2017-03-06 16:44:00 -0500

    adds poi placeholders

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
 import ol = require("openlayers");
 import { Popup } from "ol3-popup";
 import { create as CreateToolbar } from "./app/draw/toolbar";
+import { create as CreateSearch } from "./app/search";
 import { cssin, html, mixin } from "ol3-fun/ol3-fun/common";
+import { styles } from "./app/symbology";
+import { StyleConverter } from "ol3-symbolizer";
+
 import { UpperPeninsula } from "./app/poi/usa";
+
+import POI = require("./app/poi/geonames");
 
 cssin("schole-bus", `
 html, body, .schole-bus {
@@ -22,6 +28,8 @@ export function run() {
     // create map
     let map = new ol.Map({
         target: target,
+        loadTilesWhileAnimating: true,
+        loadTilesWhileInteracting: true,
         view: new ol.View({
             zoom: 10,
             center: ol.proj.transform(UpperPeninsula, "EPSG:4326", "EPSG:3857"),
@@ -31,9 +39,73 @@ export function run() {
             new ol.layer.Tile({
                 opacity: 0.8,
                 source: new ol.source.OSM()
-            })]
+            })
+        ]
     });
 
-    Popup.create({ map: map, autoPopup: false });
     CreateToolbar({ map: map });
+    CreateSearch({ map: map });
+
+    {
+        let poi = new ol.layer.Vector({
+            source: new ol.source.Vector(),
+            minResolution: 1 / 16,
+            maxResolution: 256
+        });
+        map.addLayer(poi);
+
+        let popup = Popup.create({ map: map, autoPopup: false });
+
+        let converter = new StyleConverter();
+        let styleHash = <{ [name: string]: ol.style.Style[] }>{};
+
+        poi.setStyle((feature: ol.Feature, res: number) => {
+            let symbology = <string>feature.get("poi-data").c;
+            let style = styleHash[symbology];
+            if (!style) {
+                if (!styles[symbology]) {
+                    console.log("undefined symbology:", symbology);
+                }
+                style = styleHash[symbology] = (styles[symbology] || styles["*"]).map(s => converter.fromJson(s));
+            }
+            return style;
+        });
+
+        POI.forEach(region => {
+            let features = region.markers.map(m => {
+                let g = new ol.geom.Point([parseFloat(m.lng), parseFloat(m.lat)]);
+                g.transform("EPSG:4326", "EPSG:3857");
+                let feature = new ol.Feature({
+                    "poi-data": m
+                });
+                feature.setGeometry(g);
+                return feature;
+            });
+
+            poi.getSource().addFeatures(features);
+        });
+
+        map.on("click", (args: ol.MapBrowserPointerEvent) => {
+            let location = args.pixel;
+            let extent = <ol.Extent>[location[0], location[1], location[0], location[1]];
+            extent = ol.extent.buffer(extent, 8);
+
+            // reverse y (up is down in pixel world)
+            [extent[0], extent[1]] = map.getCoordinateFromPixel([extent[0], extent[1]]);
+            [extent[2], extent[3]] = map.getCoordinateFromPixel([extent[2], extent[3]]);
+            let v = extent[1];
+            extent[1] = extent[3];
+            extent[3] = v;
+
+            poi.getSource().forEachFeatureInExtent(extent, feature => {
+                let coord = args.coordinate;
+                if (!feature.getGeometry().intersectsCoordinate(args.coordinate)) {
+                    coord = feature.getGeometry().getClosestPoint(coord);
+                }
+                popup.show(coord, JSON.stringify(feature.get("poi-data"), null, "\t"));
+                return true;
+            })
+        });
+
+    }
 }
