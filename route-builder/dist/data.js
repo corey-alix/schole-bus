46a77f464b7256aca4954e7a132de7f603eb8704
Corey Alix <coreyalix@hotmail.com>
2022-06-14 17:57:34 -0400

    draft of 2022 trip

---
+++ route-builder/dist/data.js
@@ -0,0 +1,673 @@
+export const data = [
+    {
+        lon: -82.3698745,
+        lat: 34.8333188,
+        text: "Greenville",
+    },
+    {
+        lon: -97.387217,
+        lat: 37.6853439,
+        text: "Wichita",
+    },
+    {
+        lon: -96.6421455,
+        lat: 39.8463415,
+        text: "Marysville",
+    },
+    {
+        lon: -96.6887845,
+        lat: 40.797108,
+        text: "Lincoln",
+    },
+    {
+        lon: -96.0249651,
+        lat: 41.2918233,
+        text: "Omaha",
+    },
+    {
+        lon: -90.6912553,
+        lat: 42.4845214,
+        text: "Dubuque",
+    },
+    {
+        lon: -89.3380894,
+        lat: 43.062164,
+        text: "Monona",
+    },
+    {
+        lon: -89.0289834,
+        lat: 43.3362069,
+        text: "Columbus",
+    },
+    {
+        lon: -88.5568306,
+        lat: 44.0180513,
+        text: "Oshkosh",
+    },
+    {
+        lon: -89.5722535,
+        lat: 44.5302235,
+        text: "Stevens Point",
+    },
+    {
+        lon: -87.3950903,
+        lat: 46.5537609,
+        text: "Marquette",
+    },
+    {
+        lon: -88.5753433,
+        lat: 47.1195822,
+        text: "Houghton",
+    },
+    {
+        lon: -88.5753433,
+        lat: 47.1195822,
+        text: "Houghton",
+    },
+    {
+        lon: -92.1212623,
+        lat: 46.764657,
+        text: "Duluth",
+    },
+    {
+        lon: -93.027374,
+        lat: 48.437695,
+        text: "Kabetogama",
+    },
+    {
+        lon: -93.4197129,
+        lat: 48.5855184,
+        text: "International Falls",
+    },
+    {
+        lon: -94.8913155,
+        lat: 47.478677,
+        text: "Bemidji",
+    },
+    {
+        lon: -96.7483905,
+        lat: 46.8656166,
+        text: "Moorhead",
+    },
+    {
+        lon: -100.7640799,
+        lat: 46.809081,
+        text: "Bismarck",
+    },
+    {
+        lon: -104.5162697,
+        lat: 48.1492025,
+        text: "Culbertson",
+    },
+    {
+        lon: -109.0304433,
+        lat: 48.5844436,
+        text: "Zurich",
+    },
+    {
+        lon: -111.9088208,
+        lat: 48.4294615,
+        text: "Shelby",
+    },
+    {
+        lon: -114.1870951,
+        lat: 48.371273,
+        text: "Columbia Falls",
+    },
+    {
+        lon: -116.2956682,
+        lat: 48.6910085,
+        text: "Bonners Ferry",
+    },
+    {
+        lon: -117.9026321,
+        lat: 48.5498264,
+        text: "Colville",
+    },
+    {
+        lon: -120.1766296,
+        lat: 48.472546,
+        text: "Winthrop",
+    },
+    {
+        lon: -121.5976335,
+        lat: 48.485671,
+        text: "Rockport",
+    },
+    {
+        lon: -123.4802737,
+        lat: 48.1702474,
+        text: "Port Angeles",
+    },
+    {
+        lon: -124.3854552,
+        lat: 47.9526679,
+        text: "Forks",
+    },
+    {
+        lon: -122.1597797,
+        lat: 47.5978697,
+        text: "Bellevue",
+    },
+    {
+        lon: -122.2930653,
+        lat: 47.4425995,
+        text: "SeaTac",
+    },
+    {
+        lon: -122.2677947,
+        lat: 46.8698435,
+        text: "Eatonville",
+    },
+    {
+        lon: -122.0221184,
+        lat: 46.752282,
+        text: "Ashford",
+    },
+    {
+        lon: -122.9517978,
+        lat: 46.0950105,
+        text: "Rainier",
+    },
+    {
+        lon: -122.7365002,
+        lat: 46.3248338,
+        text: "Toutle",
+    },
+    {
+        lon: -122.9725418,
+        lat: 45.300596,
+        text: "Newberg",
+    },
+    {
+        lon: -121.7828015,
+        lat: 43.2165181,
+        text: "Chemult",
+    },
+    {
+        lon: -122.4889245,
+        lat: 42.7509608,
+        text: "Prospect",
+    },
+    {
+        lon: -123.6422122,
+        lat: 42.167597,
+        text: "Cave Junction",
+    },
+    {
+        lon: -123.9695195,
+        lat: 41.8453907,
+        text: "Gasquet",
+    },
+    {
+        lon: -124.0384069,
+        lat: 41.5265091,
+        text: "Klamath",
+    },
+    {
+        lon: -122.9447474,
+        lat: 40.6520896,
+        text: "Douglas City",
+    },
+    {
+        lon: -123.2106044,
+        lat: 39.142123,
+        text: "Ukiah",
+    },
+    {
+        lon: -122.492678,
+        lat: 37.8589362,
+        text: "Sausalito",
+    },
+    {
+        lon: -123.0466412,
+        lat: 37.7291734,
+        text: "San Francisco",
+    },
+    {
+        lon: -122.3439628,
+        lat: 37.689127,
+        text: "Brisbane",
+    },
+    {
+        lon: -121.7440095,
+        lat: 38.5550857,
+        text: "Davis",
+    },
+    {
+        lon: -121.8006399,
+        lat: 36.6900785,
+        text: "Marina",
+    },
+    {
+        lon: -121.8871325,
+        lat: 36.6114267,
+        text: "Monterey",
+    },
+    {
+        lon: -121.1206161,
+        lat: 35.5935899,
+        text: "San Simeon State Park",
+    },
+    {
+        lon: -120.8499013,
+        lat: 35.3658074,
+        text: "Morro Bay",
+    },
+    {
+        lon: -120.6912456,
+        lat: 35.6267653,
+        text: "Paso Robles",
+    },
+    {
+        lon: -119.430183,
+        lat: 35.418306,
+        text: "Buttonwillow",
+    },
+    {
+        lon: -119.1244986,
+        lat: 35.3501809,
+        text: "Bakersfield",
+    },
+    {
+        lon: -118.4561967,
+        lat: 35.7068961,
+        text: "Wofford Heights",
+    },
+    {
+        lon: -118.9045446,
+        lat: 36.4388364,
+        text: "Three Rivers",
+    },
+    {
+        lon: -118.913718,
+        lat: 36.7849455,
+        text: "Hume",
+    },
+    {
+        lon: -119.5605225,
+        lat: 36.6993365,
+        text: "Sanger",
+    },
+    {
+        lon: -119.7118119,
+        lat: 36.9877273,
+        text: "Friant",
+    },
+    {
+        lon: -119.6404311,
+        lat: 37.4785497,
+        text: "Fish Camp",
+    },
+    {
+        lon: -119.6549161,
+        lat: 37.5368699,
+        text: "Wawona",
+    },
+    {
+        lon: -119.7500832,
+        lat: 37.69915,
+        text: "Yosemite National Park Road",
+    },
+    {
+        lon: -119.1218156,
+        lat: 37.9574253,
+        text: "Lee Vining",
+    },
+    {
+        lon: -118.7864946,
+        lat: 39.4765715,
+        text: "Fallon",
+    },
+    {
+        lon: -119.6365012,
+        lat: 39.1997028,
+        text: "Eureka",
+    },
+    {
+        lon: -115.579967,
+        lat: 40.74672,
+        text: "Spring Creek",
+    },
+    {
+        lon: -114.8537123,
+        lat: 42.7105204,
+        text: "Salmon Falls Creek Road",
+    },
+    {
+        lon: -114.2606001,
+        lat: 43.463518,
+        text: "Bellevue",
+    },
+    {
+        lon: -116.5290116,
+        lat: 43.3754405,
+        text: "Melba",
+    },
+    {
+        lon: -111.0368868,
+        lat: 43.7538105,
+        text: "Alta",
+    },
+    {
+        lon: -110.7182648,
+        lat: 43.6557657,
+        text: "Moose",
+    },
+    {
+        lon: -110.6681,
+        lat: 44.1055,
+        text: "44.1055,-110.6681",
+    },
+    {
+        lon: -110.6686251,
+        lat: 44.1054989,
+        text: "Flagg South Parking Lot Access Road",
+    },
+    {
+        lon: -110.6681,
+        lat: 44.1055,
+        text: "-110.6681,44.1055",
+    },
+    {
+        lon: -109.6683,
+        lat: 44.4613,
+        text: "-109.6683,44.4613",
+    },
+    {
+        lon: -106.7087319,
+        lat: 44.3445413,
+        text: "Buffalo",
+    },
+    {
+        lon: -105.5278559,
+        lat: 44.2747816,
+        text: "Gillette",
+    },
+    {
+        lon: -104.3669037,
+        lat: 44.4051485,
+        text: "Sundance",
+    },
+    {
+        lon: -103.7146053,
+        lat: 44.3874289,
+        text: "Deadwood",
+    },
+    {
+        lon: -103.6029283,
+        lat: 43.7684264,
+        text: "Custer",
+    },
+    {
+        lon: -103.4916,
+        lat: 43.4616,
+        text: "-103.4916,43.4616",
+    },
+    {
+        lon: -103.5273973,
+        lat: 43.2088005,
+        text: "Fall River County",
+    },
+    {
+        lon: -103.44,
+        lat: 43.877,
+        text: "-103.44,43.877",
+    },
+    {
+        lon: -103.4335259,
+        lat: 43.8798508,
+        text: "Grizzly Gulch Road",
+    },
+    {
+        lon: -102.2384,
+        lat: 43.919,
+        text: "-102.2384,43.919",
+    },
+    {
+        lon: -102.2387832,
+        lat: 43.9320342,
+        text: "Highway 240",
+    },
+    {
+        lon: -103.0009329,
+        lat: 42.6974486,
+        text: "Berryville Road",
+    },
+    {
+        lon: -103.0009329,
+        lat: 42.6974486,
+        text: "Berryville Road",
+    },
+    {
+        lon: -102.8745734,
+        lat: 42.0936812,
+        text: "Alliance",
+    },
+    {
+        lon: -103.6590188,
+        lat: 41.8689144,
+        text: "Scottsbluff",
+    },
+    {
+        lon: -104.741472,
+        lat: 42.2694649,
+        text: "Guernsey",
+    },
+    {
+        lon: -106.7864664,
+        lat: 42.9780764,
+        text: "Natrona County",
+    },
+    {
+        lon: -105.3390453,
+        lat: 41.1640857,
+        text: "Vedauwoo Glen Road",
+    },
+    {
+        lon: -105.0434745,
+        lat: 40.4435107,
+        text: "Loveland Recreation Trail; Loveland Bike Route - Boyd Lake West",
+    },
+    {
+        lon: -105.5961532,
+        lat: 40.4012872,
+        text: "Cascade Cottages",
+    },
+    {
+        lon: -105.8469302,
+        lat: 40.3190297,
+        text: "Trail Ridge Road",
+    },
+    {
+        lon: -105.889211,
+        lat: 40.1795796,
+        text: "County Road 41",
+    },
+    {
+        lon: -106.4226417,
+        lat: 40.3049184,
+        text: "County Highway 27",
+    },
+    {
+        lon: -108.9956511,
+        lat: 40.26029,
+        text: "County Road 161",
+    },
+    {
+        lon: -109.3348516,
+        lat: 40.3696849,
+        text: "Jensen",
+    },
+    {
+        lon: -108.8366176,
+        lat: 39.268064,
+        text: "10 8/10 Road",
+    },
+    {
+        lon: -109.6310154,
+        lat: 38.539977,
+        text: "Potash Road",
+    },
+    {
+        lon: -109.6310154,
+        lat: 38.539977,
+        text: "Potash Road",
+    },
+    {
+        lon: -109.8836492,
+        lat: 38.3881342,
+        text: "Green River Overlook Road",
+    },
+    {
+        lon: -110.1596781,
+        lat: 38.9885174,
+        text: "Green River Avenue",
+    },
+    {
+        lon: -111.1094878,
+        lat: 38.213492,
+        text: "Notom-Bullfrog Road",
+    },
+    {
+        lon: -110.905741,
+        lat: 38.3299745,
+        text: "Wayne County",
+    },
+    {
+        lon: -111.4989374,
+        lat: 37.8455146,
+        text: "Garfield County",
+    },
+    {
+        lon: -111.8841866,
+        lat: 37.2721879,
+        text: "Kane County",
+    },
+    {
+        lon: -113.24528,
+        lat: 37.2285335,
+        text: "La Verkin",
+    },
+    {
+        lon: -112.0914157,
+        lat: 36.417549,
+        text: "NF-611",
+    },
+    {
+        lon: -112.0594209,
+        lat: 36.2051588,
+        text: "Transept Trail",
+    },
+    {
+        lon: -112.0426432,
+        lat: 35.675877,
+        text: "Coconino County",
+    },
+    {
+        lon: -111.7857677,
+        lat: 35.9675898,
+        text: "old US 64",
+    },
+    {
+        lon: -111.88874,
+        lat: 35.8990686,
+        text: "Coconino Rim Road",
+    },
+    {
+        lon: -112.1333796,
+        lat: 35.9755635,
+        text: "Long Jim Loop",
+    },
+    {
+        lon: -113.9992451,
+        lat: 35.231189,
+        text: "Kingman",
+    },
+    {
+        lon: -115.5073651,
+        lat: 33.6361318,
+        text: "Red Cloud Mine Road",
+    },
+    {
+        lon: -116.4136545,
+        lat: 34.14154,
+        text: "Yucca Valley",
+    },
+    {
+        lon: -115.9585392,
+        lat: 33.2966684,
+        text: "Salton City",
+    },
+    {
+        lon: -111.6359689,
+        lat: 33.5449554,
+        text: "Coon Bluff Road",
+    },
+    {
+        lon: -108.4259761,
+        lat: 32.5487715,
+        text: "Chisom Trail",
+    },
+    {
+        lon: -106.8681419,
+        lat: 32.2877355,
+        text: "Dusty Prints Road",
+    },
+    {
+        lon: -105.8645577,
+        lat: 32.7011497,
+        text: "Otero County",
+    },
+    {
+        lon: -104.4991741,
+        lat: 31.3016851,
+        text: "Culberson County",
+    },
+    {
+        lon: -104.0249977,
+        lat: 30.3095684,
+        text: "Marfa",
+    },
+    {
+        lon: -103.0833056,
+        lat: 29.81883,
+        text: "Brewster County",
+    },
+    {
+        lon: -101.3211726,
+        lat: 29.6789321,
+        text: "Rio Grande Trail",
+    },
+    {
+        lon: -99.180267,
+        lat: 29.3365209,
+        text: "United States Highway 90",
+    },
+    {
+        lon: -96.5390695,
+        lat: 28.9449542,
+        text: "Brackenridge Parkway",
+    },
+    {
+        lon: -91.6474425,
+        lat: 30.3627409,
+        text: "Atchafalaya Basin Bridge",
+    },
+    {
+        lon: -88.7849772,
+        lat: 30.4053325,
+        text: "Ocean Springs",
+    },
+    {
+        lon: -84.1397456,
+        lat: 31.6206742,
+        text: "Chehaw Park Road",
+    },
+    {
+        lon: -83.8828521,
+        lat: 33.0260979,
+        text: "Monroe County",
+    },
+];
+//# sourceMappingURL=data.js.map
--- route-builder/dist/index.js
+++ route-builder/dist/index.js
@@ -1,11 +1,11 @@
+import { data } from "./data.js";
 export function run() {
     console.log("run");
     const map = createMap();
-    // remember this marker
     const markers = JSON.parse(localStorage.getItem("markers") ||
         "[]");
     if (!markers.length) {
-        data.forEach(d => markers.push(d));
+        data.forEach((d) => markers.push(d));
     }
     markers.forEach((marker) => addMarker({
         text: marker.text,
@@ -22,17 +22,12 @@ export function run() {
         ]);
     }
     map.on("load", () => {
+        showMarkers(markers, map);
         const polyline = [];
         markers.forEach((marker) => polyline.push([
             marker.lon,
             marker.lat,
         ]));
-        if (markers.length > 1) {
-            map.fitBounds([
-                asPoint(markers[markers.length - 2]),
-                asPoint(markers[markers.length - 1]),
-            ]);
-        }
         map.addSource("route", {
             type: "geojson",
             data: {
@@ -58,9 +53,33 @@ export function run() {
             },
         });
     });
+    const input = document.getElementById("search");
+    input.addEventListener("change", async (e) => {
+        const value = input.value;
+        // if value is a comma separated list of numbers
+        if (value.match(/^[-0-9.]+, [-0-9.]+$/)) {
+            // get the numbers
+            const numbers = value
+                .split(",")
+                .map((n) => parseFloat(n));
+            // if there are two numbers create a marker
+            if (numbers.length === 2) {
+                const marker = {
+                    lon: numbers[0],
+                    lat: numbers[1],
+                    text: value,
+                };
+                input.value = numbers
+                    .reverse()
+                    .join(",");
+            }
+        }
+    });
     const geocoder = new maptiler.Geocoder({
         input: "search",
         key: "f0zkb15NK1sqOcE72HCf",
+        language: "en",
+        bbox: [-135, 20, -80, 50],
     });
     geocoder.on("select", function (item) {
         console.log("Selected", item);
@@ -73,27 +92,55 @@ export function run() {
             lat: center[1],
             text: item.text,
         });
-        if (markers.length > 1) {
-            map.fitBounds([
-                asPoint(markers[markers.length - 2]),
-                asPoint(markers[markers.length - 1]),
-            ]);
-        }
-        else if (markers.length > 0) {
-            map.setCenter(asPoint(markers[markers.length - 1]));
-        }
         localStorage.setItem("markers", JSON.stringify(markers));
+        map.setCenter(asPoint(markers[markers.length - 1]));
+        const bounds = map
+            .getBounds()
+            .toArray();
+        if (markers.length > 0)
+            bounds.push(asPoint(markers[markers.length - 1]));
+        if (markers.length > 1)
+            bounds.push(asPoint(markers[markers.length - 2]));
+        map.fitBounds(bounds, {
+            padding: {
+                top: 10,
+                bottom: 25,
+                left: 15,
+                right: 5,
+            },
+        });
     });
+}
+function showMarkers(markers, map) {
+    if (markers.length > 1) {
+        const bounds = [
+            asPoint(markers[markers.length - 2]),
+            asPoint(markers[markers.length - 1]),
+        ];
+        map.fitBounds(bounds, {
+            padding: {
+                top: 10,
+                bottom: 25,
+                left: 15,
+                right: 5,
+            },
+        });
+    }
 }
 function addMarker(item, map) {
     const { text, center } = item;
+    const popup = new mapboxgl.Popup({
+        offset: 25,
+    }).setText(text);
     const marker = new mapboxgl.Marker({
         draggable: true,
         text: text,
     });
     // set marker text
-    marker.setLngLat(center);
-    marker.addTo(map);
+    marker
+        .setLngLat(center)
+        .setPopup(popup)
+        .addTo(map);
 }
 function createMap() {
     // creates a maptiler map
@@ -108,316 +155,20 @@ function createMap() {
 function asPoint(marker) {
     return [marker.lon, marker.lat];
 }
-const data = [
-    {
-        lon: -82.3698745,
-        lat: 34.8333188,
-        text: "Greenville",
-    },
-    {
-        lon: -97.387217,
-        lat: 37.6853439,
-        text: "Wichita",
-    },
-    {
-        lon: -96.6421455,
-        lat: 39.8463415,
-        text: "Marysville",
-    },
-    {
-        lon: -96.6887845,
-        lat: 40.797108,
-        text: "Lincoln",
-    },
-    {
-        lon: -96.0249651,
-        lat: 41.2918233,
-        text: "Omaha",
-    },
-    {
-        lon: -90.6912553,
-        lat: 42.4845214,
-        text: "Dubuque",
-    },
-    {
-        lon: -89.3380894,
-        lat: 43.062164,
-        text: "Monona",
-    },
-    {
-        lon: -89.0289834,
-        lat: 43.3362069,
-        text: "Columbus",
-    },
-    {
-        lon: -88.5568306,
-        lat: 44.0180513,
-        text: "Oshkosh",
-    },
-    {
-        lon: -89.5722535,
-        lat: 44.5302235,
-        text: "Stevens Point",
-    },
-    {
-        lon: -87.3950903,
-        lat: 46.5537609,
-        text: "Marquette",
-    },
-    {
-        lon: -88.5753433,
-        lat: 47.1195822,
-        text: "Houghton",
-    },
-    {
-        lon: -88.5753433,
-        lat: 47.1195822,
-        text: "Houghton",
-    },
-    {
-        lon: -92.1212623,
-        lat: 46.764657,
-        text: "Duluth",
-    },
-    {
-        lon: -93.027374,
-        lat: 48.437695,
-        text: "Kabetogama",
-    },
-    {
-        lon: -93.4197129,
-        lat: 48.5855184,
-        text: "International Falls",
-    },
-    {
-        lon: -94.8913155,
-        lat: 47.478677,
-        text: "Bemidji",
-    },
-    {
-        lon: -96.7483905,
-        lat: 46.8656166,
-        text: "Moorhead",
-    },
-    {
-        lon: -100.7640799,
-        lat: 46.809081,
-        text: "Bismarck",
-    },
-    {
-        lon: -104.5162697,
-        lat: 48.1492025,
-        text: "Culbertson",
-    },
-    {
-        lon: -109.0304433,
-        lat: 48.5844436,
-        text: "Zurich",
-    },
-    {
-        lon: -111.9088208,
-        lat: 48.4294615,
-        text: "Shelby",
-    },
-    {
-        lon: -114.1870951,
-        lat: 48.371273,
-        text: "Columbia Falls",
-    },
-    {
-        lon: -116.2956682,
-        lat: 48.6910085,
-        text: "Bonners Ferry",
-    },
-    {
-        lon: -117.9026321,
-        lat: 48.5498264,
-        text: "Colville",
-    },
-    {
-        lon: -120.1766296,
-        lat: 48.472546,
-        text: "Winthrop",
-    },
-    {
-        lon: -121.5976335,
-        lat: 48.485671,
-        text: "Rockport",
-    },
-    {
-        lon: -123.4802737,
-        lat: 48.1702474,
-        text: "Port Angeles",
-    },
-    {
-        lon: -124.3854552,
-        lat: 47.9526679,
-        text: "Forks",
-    },
-    {
-        lon: -122.1597797,
-        lat: 47.5978697,
-        text: "Bellevue",
-    },
-    {
-        lon: -122.2930653,
-        lat: 47.4425995,
-        text: "SeaTac",
-    },
-    {
-        lon: -122.2677947,
-        lat: 46.8698435,
-        text: "Eatonville",
-    },
-    {
-        lon: -122.0221184,
-        lat: 46.752282,
-        text: "Ashford",
-    },
-    {
-        lon: -122.9517978,
-        lat: 46.0950105,
-        text: "Rainier",
-    },
-    {
-        lon: -122.7365002,
-        lat: 46.3248338,
-        text: "Toutle",
-    },
-    {
-        lon: -122.9725418,
-        lat: 45.300596,
-        text: "Newberg",
-    },
-    {
-        lon: -121.7828015,
-        lat: 43.2165181,
-        text: "Chemult",
-    },
-    {
-        lon: -122.4889245,
-        lat: 42.7509608,
-        text: "Prospect",
-    },
-    {
-        lon: -123.6422122,
-        lat: 42.167597,
-        text: "Cave Junction",
-    },
-    {
-        lon: -123.9695195,
-        lat: 41.8453907,
-        text: "Gasquet",
-    },
-    {
-        lon: -124.0384069,
-        lat: 41.5265091,
-        text: "Klamath",
-    },
-    {
-        lon: -122.9447474,
-        lat: 40.6520896,
-        text: "Douglas City",
-    },
-    {
-        lon: -123.2106044,
-        lat: 39.142123,
-        text: "Ukiah",
-    },
-    {
-        lon: -122.492678,
-        lat: 37.8589362,
-        text: "Sausalito",
-    },
-    {
-        lon: -123.0466412,
-        lat: 37.7291734,
-        text: "San Francisco",
-    },
-    {
-        lon: -122.3439628,
-        lat: 37.689127,
-        text: "Brisbane",
-    },
-    {
-        lon: -121.7440095,
-        lat: 38.5550857,
-        text: "Davis",
-    },
-    {
-        lon: -121.8006399,
-        lat: 36.6900785,
-        text: "Marina",
-    },
-    {
-        lon: -121.8871325,
-        lat: 36.6114267,
-        text: "Monterey",
-    },
-    {
-        lon: -121.1206161,
-        lat: 35.5935899,
-        text: "San Simeon State Park",
-    },
-    {
-        lon: -120.8499013,
-        lat: 35.3658074,
-        text: "Morro Bay",
-    },
-    {
-        lon: -120.6912456,
-        lat: 35.6267653,
-        text: "Paso Robles",
-    },
-    {
-        lon: -119.430183,
-        lat: 35.418306,
-        text: "Buttonwillow",
-    },
-    {
-        lon: -119.1244986,
-        lat: 35.3501809,
-        text: "Bakersfield",
-    },
-    {
-        lon: -118.4561967,
-        lat: 35.7068961,
-        text: "Wofford Heights",
-    },
-    {
-        lon: -118.9045446,
-        lat: 36.4388364,
-        text: "Three Rivers",
-    },
-    {
-        lon: -118.913718,
-        lat: 36.7849455,
-        text: "Hume",
-    },
-    {
-        lon: -119.5605225,
-        lat: 36.6993365,
-        text: "Sanger",
-    },
-    {
-        lon: -119.7118119,
-        lat: 36.9877273,
-        text: "Friant",
-    },
-    {
-        lon: -119.6404311,
-        lat: 37.4785497,
-        text: "Fish Camp",
-    },
-    {
-        lon: -119.6549161,
-        lat: 37.5368699,
-        text: "Wawona",
-    },
-    {
-        lon: -119.7500832,
-        lat: 37.69915,
-        text: "Yosemite National Park Road",
-    },
-];
+function promptForAccessKey() {
+    if (!localStorage.getItem("accessKey")) {
+        const key = prompt("Please enter your access key:", "");
+        if (key) {
+            localStorage.setItem("accessKey", key);
+        }
+    }
+    return localStorage.getItem("accessKey");
+}
+async function geoLocate(address) {
+    const accessKey = promptForAccessKey();
+    const url = `http://open.mapquestapi.com/geocoding/v1/address?key=${accessKey}&location=${address}`;
+    const location = await fetch(url);
+    const data = (await location.json());
+    return data;
+}
 //# sourceMappingURL=index.js.map
